1b9cdafe18f35effd234359a63e65477a92f5839
khannhDanng <hodackhanhdang26@gmail.com>
2024-12-14 13:29:20 +0700

    Update RedirectIfAuthenticated.php

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 class RedirectIfAuthenticated
 {
     /**
