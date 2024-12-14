310f04b0920137b67f26214f74281ea4d01484d8
khannhDanng <hodackhanhdang26@gmail.com>
2024-12-14 13:29:04 +0700

    Update PreventRequestsDuringMaintenance.php

--- app/Http/Middleware/PreventRequestsDuringMaintenance.php
+++ app/Http/Middleware/PreventRequestsDuringMaintenance.php
@@ -1,9 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
 use Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance as Middleware;
-
 class PreventRequestsDuringMaintenance extends Middleware
 {
     /**
